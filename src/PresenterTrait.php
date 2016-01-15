80a4a8e3a923fee9ce4b8cf208c70a4a2d9e1ee4
patrickkusebauch <patrick.kusebauch@gmail.com>
2016-01-15 13:44:45 +0100

    Legacy

--- src/PresenterTrait.php
+++ src/PresenterTrait.php
@@ -2,28 +2,23 @@
 
 namespace Kusebauch\NetteSecurityAnnotation;
 
-use Nette\Application\ForbiddenRequestException;
-use Nette\InvalidStateException;
-use Nette\Reflection\ClassType;
-use Nette\Reflection\Method;
-
 trait PresenterTrait
 {
     public function checkRequirements($element)
     {
-        if ($element instanceof ClassType) {return;}; //not checking class access, only method access
+        if ($element instanceof \ClassReflection) {return;}; //not checking class access, only method access
         $user = $this->user;
         // Allowing for both method level and class level annotations
-        $class = ($element instanceof Method) ? $element->getDeclaringClass() : $element;
+        $class = ($element instanceof \MethodReflection) ? $element->getDeclaringClass() : $element;
         $secured = $element->getAnnotation('Secured') || $class->getAnnotation('Secured');
 
         if ($secured) {
             if (!$user->isLoggedIn()) {
-                throw new ForbiddenRequestException("User has to be logged in to access secured presenter.");
+                throw new \ForbiddenRequestException("User has to be logged in to access secured presenter.");
             } else {
                 //check existence of resource
                 if (!($element->hasAnnotation('Resource') || $class->hasAnnotation('Resource'))) {
-                    throw new InvalidStateException('Secured method is missing a resource.');
+                    throw new \InvalidStateException('Secured method is missing a resource.');
                 }
                 $resource = $element->hasAnnotation('Resource') ? $element->getAnnotation('Resource') : $class->getAnnotation('Resource');
                 $privileges = array_merge((array) $class->getAnnotation('Privilege'), (array) $element->getAnnotation('Privilege'));
@@ -32,7 +27,7 @@ trait PresenterTrait
                     if ($user->isAllowed($resource, $privilege)) $allowed = TRUE;
                 }
                 if (!$allowed) {
-                    throw new ForbiddenRequestException("User is not allowed to access resource '$resource'");
+                    throw new \ForbiddenRequestException("User is not allowed to access resource '$resource'");
                 }
             }
 
@@ -51,7 +46,7 @@ trait PresenterTrait
 
             $annotations = (array) $reflection->getAnnotation('Action');
             if (!empty($annotations) && !in_array($this->getAction(), $annotations)) {
-                throw new ForbiddenRequestException("Creation of component '$name' is forbidden for action '$this->action'.");
+                throw new \ForbiddenRequestException("Creation of component '$name' is forbidden for action '$this->action'.");
             }
         }
     }
